f884bbe1073d348d926ba0faf07157e019e13fc5
Rakesh Bhavanand <148518848+RakeshBhavanand24@users.noreply.github.com>
2023-11-14 20:39:49 +0530

    Add files via upload

---
+++ KSP_task-5/script.js
@@ -0,0 +1,46 @@
+
+// Alert methods---
+window.alert("Enter the details correctly");
+
+
+// let variable---
+let num=15;
+
+// console.log(to print the string);
+
+console.log(num);
+
+window.alert("Enter the Name");
+
+//const variable---
+const num1=20;
+console.log(num1);
+
+
+
+// CAMEL CASE:: {single line comment}
+
+/* ((camelcase "In camelcase all the first element from the word  of a particular sentence 
+ expect first element from first word turns into capital and removes space between the words "))
+
+
+normal case :: Age of a person=32
+
+ camel case :: ageOfAPerson=32*/     /*{multi line comments}*/
+
+
+
+
+
+//  opeartors:
+
+// operators are used to do operations of two are more variables
+
+let n=12;
+const v=34;
+console.log(n+v);
+
+
+
+
+
